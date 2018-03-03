7c0c0df64ccd26c60886d26a0715525d22b06981
Luis Hernandez <luis.antonio@tcpip.tech>
2018-03-02 20:09:22 -0600

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} ad link: ${link.value}`);
+    return false;
+  }
+
+}
