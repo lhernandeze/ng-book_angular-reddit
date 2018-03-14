771e6e72c000afee828e60e3be0c42b8f2e9e025
Luis Hernandez <luis.antonio@tcpip.tech>
2018-03-13 20:01:01 -0600

    Add changes to styles test

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,19 +1,17 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
 import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.scss']
 })
 export class ArticleComponent implements OnInit {
 
   @HostBinding('attr.class') cssClass='row';
-  article: Article;
+  @Input() article: Article;
 
   constructor() {
-    this.article = new Article("Angular 5",
-    "http://angular.io", 10);
   }
 
   voteUp(): boolean {
